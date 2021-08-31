77a9238d7b516d8c7ccbdd41ee4a848f03112c25
Junior Didzec <junior_didzec@hotmail.com>
2021-08-31 13:19:36 -0300

    Finish form

--- js/components/send-form.js
+++ js/components/send-form.js
@@ -4,5 +4,21 @@ let inputEmail = document.querySelector('#input-email').value;
 let inputMassage = document.querySelector('#input-message').value;
 
 btn.addEventListener('click', function() {
-    
+    sendEmail(inputName, inputEmail, inputMassage);
 });
+
+function sendEmail(inputName, inputEmail, inputMassage) {
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "didzecjr@gmail.com",
+        Password: "oodoryxlwaczoxrj",
+        To: "junior_didzec@hotmail.com",
+        From: "didzecjr@gmail.com",
+        Subject: `${inputName} mandou uma mensagem para você`,
+        Body: `Name: ${inputName} <br/> Email: ${inputEmail} <br/> Message: ${inputMassage}`,
+    }).then((inputMassage) => alert("E-mail enviado com sucesso"));
+
+    inputName = '';
+    inputEmail= '';
+    inputMassage= '';
+}
