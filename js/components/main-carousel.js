7ff6f1927e57770709e282da6fa4cff0db7acf40
Junior Didzec <junior_didzec@hotmail.com>
2021-09-06 17:45:31 -0300

    Change form

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -2,6 +2,7 @@ const btnRight = document.querySelector('.button-arrow.-right');
 const btnleft = document.querySelector('.button-arrow.-left');
 const items = document.querySelector('.items');
 let currentPixelIndex = 0;
+items.scrollTo(0, 0);
 
 btnRight.addEventListener('click', function() {
     if (currentPixelIndex <= 3000){
